350e56fc7bd1c0d826f908bad2efcd9197ad18f2
JingyuKim <swc05161@gmail.com>
2023-10-17 11:54:44 +0900

    feat: 1181 단어 정렬

---
+++ 1주차/NO1181.py
@@ -0,0 +1,12 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+words = set()
+
+for _ in range(N):
+    words.add(sys.stdin.readline().rstrip())
+
+new_words = sorted(words, key=lambda w: (len(w), w))
+
+for word in new_words:
+    print(word)
