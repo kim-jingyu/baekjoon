4fafca484cca685a9c0d6dd2a088e037c4c62242
JingyuKim <swc05161@gmail.com>
2023-10-23 18:15:21 +0900

    2573 빙산

--- 2주차/2573.py
+++ 2주차/2573.py
@@ -1,65 +1,63 @@
 import sys
-import copy
+from collections import deque
+sys.setrecursionlimit(10 ** 6)
 
 input = lambda: sys.stdin.readline().rstrip()
 
 N, M = map(int, input().split())  # 행, 열
-
 board = [list(map(int, input().split())) for _ in range(N)]
-new_board = copy.deepcopy(board)
-temp_board = copy.deepcopy(board)
 
-answer = 0
-check_global = True
-
-dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+year = 0
 
 
-while check_global:
-    def remove_part(calc_board, y, x):
-        global temp_board
+# 빙산의 위치 i, j를 받아와서 빙산 주변 바다 갯수 카운트
+def bfs(y, x):
+    queue = deque([(y, x)])
+    visited[y][x] = True
+
+    while queue:
+        y, x = queue.popleft()
+
         for k in range(4):
             ny = y + dy[k]
             nx = x + dx[k]
-            if ny < 0 or ny == N or nx < 0 or nx == M:
-                continue
 
-            if calc_board[ny][nx]:
-                calc_board[ny][nx] = 0
-                remove_part(calc_board, ny, nx)
+            if 0 <= ny < N and 0 <= nx < M and not visited[ny][nx]:
+                # 빙산이라면 큐에 추가
+                if board[ny][nx]:
+                    visited[ny][nx] = True
+                    queue.append((ny, nx))
+                # 빙산이 아니라면 바닷물로 카운트
+                else:
+                    cnt[y][x] += 1
 
-        temp_board = copy.deepcopy(calc_board)
-
-
-    def calculate_num_of_lumps(calc_board):
-        global answer, check_global
-        check = False
-        for i in range(N):
-            for j in range(M):
-                if calc_board[i][j]:
-                    check = True
-                    calc_board[i][j] = 0
-                    answer += 1
-                    remove_part(calc_board, i, j)
-        check_global = check
+    return 1
 
 
-    def decrease_iceberg(y, x):
-        for k in range(4):
-            ny = y + dy[k]
-            nx = x + dx[k]
-            if ny < 0 or ny == N or nx < 0 or nx == M:
-                continue
+while True:
+    answer = []
+    visited = [[False] * M for _ in range(N)]
+    cnt = [[0] * M for _ in range(N)]
 
-            if new_board[ny][nx] == 0 and temp_board[y][x] > 0:
-                temp_board[y][x] -= 1
-
-        calculate_num_of_lumps(temp_board)
-
-
+    # 빙산과 접촉되어 있는 바닷물 확인
     for i in range(N):
         for j in range(M):
-            decrease_iceberg(i, j)
+            if board[i][j] and not visited[i][j]:
+                answer.append(bfs(i, j))
 
-print(answer)
+    # 빙산 깍아주기
+    for i in range(N):
+        for j in range(M):
+            board[i][j] -= cnt[i][j]
+            if board[i][j] < 0:
+                board[i][j] = 0
+
+    # 빙산 그룹이 없거나 2 덩어리 이상이라면
+    if len(answer) == 0 or len(answer) >= 2:
+        break
+
+    year += 1
+
+print(year) if len(answer) >= 2 else print(0)
