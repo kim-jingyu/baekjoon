f33b084951ebc7582f0ca254b474b149c86274d3
JingyuKim <swc05161@gmail.com>
2023-10-30 22:26:20 +0900

    feat: 9048 동전

---
+++ 3주차/9084.py
@@ -0,0 +1,22 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    coins = list(map(int, input().split()))
+    M = int(input())
+
+    dp = [0] * (M + 1)
+
+    for coin in coins:
+        if coin > M:
+            continue
+        dp[coin] += 1
+        for i in range(coin, M + 1):
+            if i >= coin:
+                dp[i] += dp[i-coin]
+
+    print(dp[M])
