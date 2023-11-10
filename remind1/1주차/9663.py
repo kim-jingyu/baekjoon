691dfb1cef3f5fca85a0e2de6adef64db050eb5e
kim-jingyu <swc05161@gmail.com>
2023-11-10 13:52:18 +0900

    feat: 9663 N퀸 문제 해결

--- remind1/1주차/9663.py
+++ remind1/1주차/9663.py
@@ -4,27 +4,24 @@ input = lambda: sys.stdin.readline().rstrip()
 
 N = int(input())
 
-pos = [0] * N
-flag_for_y = [False] * N
+result = 0
+flag_for_x = [0] * N
+flag_for_diagonal_left = [0] * (2 * N - 1)
+flag_for_diagonal_right = [0] * (2 * N - 1)
 
 
-def put():
-    for i in range(N):
-        print(f'{pos[i]:2}', end='')  # :2 문자열 자릿수
-    print()
-
-
-def set(i):
-    if i == N:
-        put()
+def calculate(y):
+    global result
+    if y == N:
+        result += 1
         return
 
-    for j in range(N):
-        if not flag_for_y[j]:
-            pos[i] = j
-            flag_for_y[j] = True
-            set(i + 1)
-            flag_for_y[j] = False
+    for x in range(N):
+        if not flag_for_x[x] and not flag_for_diagonal_left[y + x] and not flag_for_diagonal_right[y - x + N - 1]:
+            flag_for_x[x] = flag_for_diagonal_left[y + x] = flag_for_diagonal_right[y - x + N - 1] = True
+            calculate(y + 1)
+            flag_for_x[x] = flag_for_diagonal_left[y + x] = flag_for_diagonal_right[y - x + N - 1] = False
 
 
-set(0)
+calculate(0)
+print(result)
